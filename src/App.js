586694d850c1f132591d21bdb79b8d09247866c7
Minhhari <minhhailk2004@gmail.com>
2025-05-14 22:16:07 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Chào mừng bạn đến với React!</h1>
+      <h2>Hello React</h2>
+    </div>
+  );
+}
+
+export default App;
